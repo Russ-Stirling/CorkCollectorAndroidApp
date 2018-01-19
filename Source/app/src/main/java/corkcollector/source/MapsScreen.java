a335b82244ebcc004a55d4885409b1cffb460b22
bhanna4 <baileyhanna17@gmail.com>
2018-01-18 19:12:10 -0500

    Menus appear and are navigable for all screens except maps (requries more work due to complexity of FragmentActivity

--- Source/app/src/main/java/corkcollector/source/MapsScreen.java
+++ Source/app/src/main/java/corkcollector/source/MapsScreen.java
@@ -1,7 +1,11 @@
 package corkcollector.source;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,5 +46,36 @@ public class MapsScreen extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.item1:
+                Intent myIntent = new Intent(MapsScreen.this,
+                        MapsScreen.class);
+                startActivity(myIntent);
+                break;
+            case R.id.item2:
+                Intent myIntent2 = new Intent(MapsScreen.this,
+                        WineryScreen.class);
+                startActivity(myIntent2);
+                break;
+            case R.id.item3:
+                Intent myIntent3 = new Intent(MapsScreen.this,
+                        WineScreen.class);
+                startActivity(myIntent3);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
