9d7df7512a4868c6c4310ae8c25f1f3ffe0ba3fb
dlorence <daniel.lorencez@gmail.com>
2018-01-29 15:55:45 -0500

    GET/PUT/POST Finalized
    Added functionality for all 3 types of requests on the request screen. Only way to test them at present is to check the console log after button presses.

--- Source/app/src/main/java/corkcollector/source/RequestScreen.java
+++ Source/app/src/main/java/corkcollector/source/RequestScreen.java
@@ -3,14 +3,25 @@ package corkcollector.source;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Daniel on 2018-01-29.
@@ -25,7 +36,7 @@ public class RequestScreen extends AppCompatActivity {
         setContentView(R.layout.request_screen);
 
         // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
+        final RequestQueue queue = Volley.newRequestQueue(this);
 
         //Grab buttons from request screen
         Button getButton = findViewById(R.id.getButton);
@@ -37,8 +48,33 @@ public class RequestScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                //Send get request
-                String url ="http://www.google.com";
+                //Send get request to sample website
+                String url = "http://httpbin.org/get?param1=hello";
+
+                //Prepare the get Request
+                JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                        new Response.Listener<JSONObject>()
+                        {
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                //Print response in log if successful
+                                Log.d("Response", response.toString());
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                                //Print "oh no!" in log if unsuccessful
+                                Log.d("Error.Response", "oh no!");
+                            }
+                        }
+                );
+
+                //Add it to the RequestQueue and send automatically
+                queue.add(getRequest);
 
             }
         });
@@ -47,7 +83,46 @@ public class RequestScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                //Send put request
+                //Send put request to sample website
+                String url = "http://httpbin.org/put";
+
+                //Prepare the put request
+                StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+
+                                //Print response in log if successful
+                                Log.d("Response", response);
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                                //Print "oh no!" in log if unsuccessful
+                                Log.d("Error.Response", "oh no!");
+                            }
+                        }
+                ) {
+
+                    //Create sample data for the put request
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("name", "Alif");
+                        params.put("domain", "http://itsalif.info");
+
+                        return params;
+                    }
+
+                };
+
+                //Add it to the request queue and send automatically
+                queue.add(putRequest);
 
             }
         });
@@ -56,7 +131,45 @@ public class RequestScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                //Send post request
+                //Send post request to sample website
+                String url = "http://httpbin.org/post";
+
+                //Prepare the post request
+                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+
+                                //Print response in log if successful
+                                Log.d("Response", response);
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                                //Print "oh no!" in log if unsuccessful
+                                Log.d("Error.Response", "oh no!");
+                            }
+                        }
+                ) {
+
+                    //Create sample data for the post request
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("name", "Alif");
+                        params.put("domain", "http://itsalif.info");
+
+                        return params;
+                    }
+                };
+
+                //Add it to the request queue and send automatically
+                queue.add(postRequest);
 
             }
         });
