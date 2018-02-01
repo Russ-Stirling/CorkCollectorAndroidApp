bf2e6024a94bdf7a0eec4408d549780ab09754f3
dlorence <daniel.lorencez@gmail.com>
2018-02-01 13:28:41 -0500

    Pins now direct to Winery Page
    When any pin on the map screen is pressed by a user, it will take the app to the winery screen.

--- Source/app/src/main/java/corkcollector/source/MapsScreen.java
+++ Source/app/src/main/java/corkcollector/source/MapsScreen.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,11 +18,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsScreen extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsScreen extends AppCompatActivity implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
 
+    //The map itself
     private GoogleMap mMap;
+
+    //The pins that will be stored and placed on the map
+    private Marker mNiagara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +38,6 @@ public class MapsScreen extends AppCompatActivity implements OnMapReadyCallback 
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -45,6 +50,7 @@ public class MapsScreen extends AppCompatActivity implements OnMapReadyCallback 
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
         /*Map Setup*/
@@ -65,9 +71,17 @@ public class MapsScreen extends AppCompatActivity implements OnMapReadyCallback 
 
         /*Placing Pins on the map*/
 
+        // TO DO: Here is where all the loading requests will take place
+        // Need to grab latitude, longitude and some sort of ID number
+        // Iterate through DB and generate all pins dynamically
+        // Is there some way to cache them?
+
         // Add a marker in Niagara on the lake
-        LatLng niagara = new LatLng(43.2550, -79.0773);
-        mMap.addMarker(new MarkerOptions().position(niagara).title("Marker in Niagara"));
+        LatLng lNiagara = new LatLng(43.2550, -79.0773);
+        mNiagara = mMap.addMarker(new MarkerOptions().position(lNiagara).title("Marker in Niagara"));
+
+        // Set a listener for marker click.
+        mMap.setOnMarkerClickListener(this);
 
         /*Disabling POIs*/
         try {
@@ -77,7 +91,7 @@ public class MapsScreen extends AppCompatActivity implements OnMapReadyCallback 
                     MapStyleOptions.loadRawResourceStyle(
                             this, R.raw.style_json));
 
-            //Post error message to log if unsuccessful
+            // Post error message to log if unsuccessful
             if (!success) {
                 Log.d("oh no!", "Style parsing failed.");
             }
@@ -85,6 +99,21 @@ public class MapsScreen extends AppCompatActivity implements OnMapReadyCallback 
             Log.e("oh no!", "Can't find style. Error: ", e);
         }
 
+    }
+
+    /** Called when the user clicks a marker. */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Load the default winery screen
+        Intent myIntent2 = new Intent(MapsScreen.this,
+                WineryScreen.class);
+        startActivity(myIntent2);
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
     }
 
     @Override
