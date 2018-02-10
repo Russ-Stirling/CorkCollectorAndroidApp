68b400c8e100590d472d34bb7f4c30d8042b0e16
bhanna4 <baileyhanna17@gmail.com>
2018-02-10 18:30:27 -0500

    Wine Cellar pop up is complete and has a non-functioning button to 'drink' the wine

--- Source/app/src/main/java/corkcollector/source/WineCellarPop.java
+++ Source/app/src/main/java/corkcollector/source/WineCellarPop.java
@@ -1,24 +1,21 @@
 package corkcollector.source;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.TextView;
 
 /**
  * Created by Bailey on 2/10/2018.
  */
 
-public class VisitedWineriesPop extends Activity {
+public class WineCellarPop extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.visited_wineries_popup_window);
+        setContentView(R.layout.wine_cellar_popup_window);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
