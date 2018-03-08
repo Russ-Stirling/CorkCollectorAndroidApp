ef008c7e65cf79dec5d704153604694f27ef62ad
dlorence <daniel.lorencez@gmail.com>
2018-03-07 19:02:16 -0500

    Dates Formatted
    For some reason, the months are coming out wrong. It works though.

--- Source/app/src/main/java/corkcollector/source/VisitedWineriesPop.java
+++ Source/app/src/main/java/corkcollector/source/VisitedWineriesPop.java
@@ -25,6 +25,10 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.OffsetDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -113,20 +117,36 @@ public class VisitedWineriesPop extends Activity {
 
         for(int visitListIndex = 0; visitListIndex < visitListSize; visitListIndex++)
         {
-            final GridLayout wineGrid = new GridLayout(this);
-            wineGrid.setRowCount(1);
-            wineGrid.setColumnCount(2);
+            final GridLayout wineryGrid = new GridLayout(this);
+            wineryGrid.setRowCount(1);
+            wineryGrid.setColumnCount(2);
 
             final TextView wineryNameTextView = new TextView(this);
+            final TextView visitDateTextView = new TextView(this);
 
             try {
-
-                //String visitDate = visitList.getJSONObject(visitListIndex).getString("visitTime");
-                //TODO: this a datetime offset object
 
                 //Set text and style of textview components
                 wineryNameTextView.setText(visitList.getJSONObject(visitListIndex).getString("wineryName"));
                 wineryNameTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                String visitDate = visitList.getJSONObject(visitListIndex).getString("visitTime");
+                visitDate = visitDate.substring(0, 10);
+                SimpleDateFormat originalFormat = new SimpleDateFormat("yyyy-mm-dd");
+                SimpleDateFormat targetFormat = new SimpleDateFormat("MMMM dd, yyyy");
+                String formattedDate = "Error";
+
+                try{
+                    Date date = originalFormat.parse(visitDate);
+                    formattedDate = targetFormat.format(date);
+                }
+
+                catch (ParseException e){
+                    e.printStackTrace();
+                }
+
+                visitDateTextView.setText(formattedDate);
+                //visitDateTextView.setGravity(Gravity.CENTER_HORIZONTAL);
 
                 //Set layout parameters of winery's name
                 GridLayout.LayoutParams wineryNameParams = new GridLayout.LayoutParams();
@@ -138,15 +158,26 @@ public class VisitedWineriesPop extends Activity {
                 wineryNameParams.setGravity(Gravity.CENTER_HORIZONTAL);
                 wineryNameTextView.setLayoutParams(wineryNameParams);
 
+                //Set layout parameters of visit date
+                GridLayout.LayoutParams visitDateParams = new GridLayout.LayoutParams();
+                visitDateParams.height = GridLayout.LayoutParams.WRAP_CONTENT;
+                visitDateParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200, getResources().getDisplayMetrics());
+                visitDateParams.topMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, getResources().getDisplayMetrics());
+                visitDateParams.rowSpec = GridLayout.spec(0, 1);
+                visitDateParams.columnSpec = GridLayout.spec(1, 1);
+                //visitDateParams.setGravity(Gravity.CENTER_HORIZONTAL);
+                visitDateTextView.setLayoutParams(visitDateParams);
+
                 //Add textviews to the grid layout
-                wineGrid.addView(wineryNameTextView);
+                wineryGrid.addView(wineryNameTextView);
+                wineryGrid.addView(visitDateTextView);
 
                 //Add the grid layout to the screen
-                visitedWineriesLinearLayout.addView(wineGrid);
+                visitedWineriesLinearLayout.addView(wineryGrid);
 
                 //Create a new onclick listener for the wine grid object
                 final String tempID = visitList.getJSONObject(visitListIndex).getString("wineryId");
-                wineGrid.setOnClickListener(new View.OnClickListener() {
+                wineryGrid.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
 
